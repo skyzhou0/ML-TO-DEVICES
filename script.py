339a7f1fcb75fc8a6236a114004c654e962396aa
Zhou <hazho@38f9d33a99e5.ant.amazon.com>
2019-08-31 23:16:42 +0800

    remove codes

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ import sys
 import requests
 
 # print(sys.version)
-print(sys.executable)
+# print(sys.executable)
 
 
 def greet(who_to_greet):
@@ -15,5 +15,4 @@ def greet(who_to_greet):
 
 r = requests.get("https://google.com")
 print(r.status_code)
-# print(greet("World"))
-# print(greet("Corey"))
+
